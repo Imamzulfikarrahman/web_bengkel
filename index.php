b8575be0498cb6c7520c1552cbf68e8895a28324
Imamzulfikarrahman <zimam8375@gmail.com>
2026-06-10 22:08:00 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!-- Menu Utama Navigasi -->
+<html>
+<body>
+<h1>Menu Utama Bengkel</h1>
+<a href='pendaftaran_pelanggan.php'>Daftar Pelanggan</a> | 
+<a href='list_pelanggan.php'>Cek Member</a> | 
+<a href='transaksi_bengkel.php'>Kasir</a> | 
+<a href='dashboard_stok.php'>Stok Barang</a>
+</body>
+</html>
